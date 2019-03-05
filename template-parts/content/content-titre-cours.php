70073bca5e55f81400da38ce508b8c8e6f3f8eb1
jeffxsi <jfbranchaud40@gmail.com>
2019-03-05 12:15:07 -0500

    ajout couleurs

--- template-parts/content/content-titre-cours.php
+++ template-parts/content/content-titre-cours.php
@@ -13,17 +13,17 @@
 		<?php
 		switch(substr(get_the_title(),4,1)){
 
-			case 1:the_title( sprintf( '<h2 class="box session1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 1:the_title( sprintf( '<h2 class="box annee1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
-			case 2:the_title( sprintf( '<h2 class="box"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 2:the_title( sprintf( '<h2 class="box annee2"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
-			case 3:the_title( sprintf( '<h2 class="box"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 3:the_title( sprintf( '<h2 class="box annee3"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
-			case 4:the_title( sprintf( '<h2 class="box"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 4:the_title( sprintf( '<h2 class="box annee4"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
-			case 5:the_title( sprintf( '<h2 class="box"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 5:the_title( sprintf( '<h2 class="box annee5"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
-			case 6:the_title( sprintf( '<h2 class="box"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		case 6:the_title( sprintf( '<h2 class="box annee6"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			break;
 
 		}
